22439647f59982b83c4e1907d2622f4046c21392
Carolina <mamanicarolina27@gmail.com>
2024-04-28 15:12:44 -0300

    Video 38 minutos aun falta

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import openai
 import config
+import typer
+from rich import print # para mejorar lo visual
+from rich.table import Table
 
-openai.api_key = config.api_key
-
-#contexto del asistente, es como guiarlo para donde quieres que vaya o su rol de lo que hara
-messages = [{"role":"system",
-             "content":"Eres un asistente muy util"}]
-
-while True: # con esto podemos seguir hablando con el
-    content = input('sobre que quieres hablar?') #el usuario introduce la pregunta
-
-    if content is "exit": #para poder parar el programa
-        break
-
-    messages.append({"role":"user","content":content})
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages)
+def main():
     
-    response_contect = response.choices[0].message.content 
+    openai.api_key = config.api_key
     
-    messages.append({"role":"assistant","content":response_contect}) #guarda la respuesta que no da
-    #asi tendra contexto de todas las preguntas que le hacemos
-    # y de todas las respuestas que nos da
+    print('[bold green]ChatGPT API en Python[/bold green]')
     
-    print(response_contect)
+    table = Table('Comando','Descripcion')
+    table.add_row('exit','salir de la app')
+    table.add_row('new','crear una nueva conversacion')
+    
+    print(table)
+
+    #contexto del asistente, es como guiarlo para donde quieres que vaya o su rol de lo que hara
+    messages = [{"role":"system",
+                "content":"Eres un asistente muy util"}]
+
+    while True: # con esto podemos seguir hablando con el
+        content = input('sobre que quieres hablar?: ') #el usuario introduce la pregunta
+
+        if content == "exit": #para poder parar el programa
+            break
+
+        messages.append({"role":"user","content":content})
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages)
+        
+        response_contect = response.choices[0].message.content 
+        
+        messages.append({"role":"assistant","content":response_contect}) #guarda la respuesta que no da
+        #asi tendra contexto de todas las preguntas que le hacemos
+        # y de todas las respuestas que nos da
+        
+        print(response_contect)
+
+if __name__ == "__main__":
+    typer.run(main)
